4bc7829bdd9bfa14191d7c5f5f75f7ab716d3d77
zhang4417 <1804815164@qq.com>
2020-10-18 19:48:05 +0800

    安装styled-components

--- src/view/Money.tsx
+++ src/view/Money.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 import Nav from "../components/Nav";
+
+const Wrapper = styled.div`
+    height:100vh;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+`
+const Main = styled.main`
+    flex-grow:1
+`
 
 function Money() {
     return (
-        <>
-            <h1>money</h1>
+        <Wrapper>
+            <Main>
+                <h1>money</h1>
+            </Main>
             <Nav />
-        </>
+        </Wrapper>
     )
 }
 
