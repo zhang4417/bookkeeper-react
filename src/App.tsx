e01a8373b2f4bf0be4513a595960e59425d694a5
zhang4417 <1804815164@qq.com>
2020-10-18 18:46:28 +0800

    安装react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/other">
+            <Other />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+          <li>
+            <Link to="/other">other</Link>
+          </li>
+          <li>
+            <Link to="/home">home</Link>
+          </li>
+        </ul>
+      </nav>
+    </Router>
   );
 }
 
-export default App;
+function About() {
+  return (<h1>about</h1>)
+}
+function Other() {
+  return (<h1>other</h1>)
+}
+function Home() {
+  return (<h1>home</h1>)
+}
+function NoMatch() {
+  return (<h1>404</h1>)
+}
+
+export default App
+
+
