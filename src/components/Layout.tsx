1790b0ced1768f42308fcbd5952d7c48f078633a
zhang4417 <1804815164@qq.com>
2020-10-20 02:11:22 +0800

    完成money基本布局

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,9 @@ const Wrapper = styled.div`
     justify-content:center;
 `
 const Main = styled.main`
-    flex-grow:1
+    flex-grow:1;
+    display:flex;
+    flex-direction:column;
 `
 
 function Layout(props: any) {
